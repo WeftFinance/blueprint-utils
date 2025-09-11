e73187ab468a2b2a98f9482a4c1bc0498110e90a
atoumbre <atoumbre@gmail.com>
2025-09-11 16:26:10 +0000

    adding safe_arith to common prelude

--- libraries/common/src/prelude.rs
+++ libraries/common/src/prelude.rs
@@ -5,5 +5,6 @@ pub use crate::service_manager::*;
 pub use crate::{generate_service_variants, updatable_config};
 
 pub use generate_config::GenerateConfig;
+pub use safe_arithmetic::safe_arith;
 pub use safe_arithmetic::safe_arithmetic;
 pub use utils::prelude::*;
