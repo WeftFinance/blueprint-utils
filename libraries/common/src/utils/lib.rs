f21a906a111d2b7e984c419434e5b53fbefdac05
atoumbre <atoumbre@gmail.com>
2025-11-22 03:39:53 -0500

    dependencies cleanup

--- libraries/common/src/utils/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod functions;
-pub mod macros;
-pub mod prelude;
-pub mod traits;
-pub mod types;
--- libraries/common/src/utils/types.rs
+++ libraries/common/src/utils/types.rs
@@ -1,4 +1,43 @@
+use super::traits::CanBeChecked;
 use scrypto::prelude::*;
 
 #[derive(ScryptoSbor, NonFungibleData)]
 pub struct EmptyBadgeData {}
+
+/// Define options for deposit limit
+#[derive(ScryptoSbor, ManifestSbor, Debug, Default, Clone, PartialEq, Eq, Hash)]
+pub enum DepositLimitType {
+  /// No limit
+  #[default]
+  None,
+
+  /// Limit by amount
+  Amount(Decimal),
+
+  /// Limit by a ratio of the resource total supply
+  SupplyRatio(Decimal),
+}
+impl DepositLimitType {
+  pub fn check(&self) -> bool {
+    match self {
+      DepositLimitType::None => true,
+      DepositLimitType::Amount(amount) => amount.is_zero_or_positive(),
+      DepositLimitType::SupplyRatio(ratio) => ratio.is_a_rate(),
+    }
+  }
+
+  pub fn check_limit(&self, res_address: ResourceAddress, value: Decimal) -> bool {
+    match self {
+      DepositLimitType::None => true,
+      DepositLimitType::Amount(amount) => value <= *amount,
+      DepositLimitType::SupplyRatio(ratio) => {
+        let res_manager: ResourceManager = res_address.into();
+        if let Some(total_supply) = res_manager.total_supply() {
+          value <= total_supply.checked_mul(*ratio).unwrap()
+        } else {
+          true
+        }
+      }
+    }
+  }
+}
